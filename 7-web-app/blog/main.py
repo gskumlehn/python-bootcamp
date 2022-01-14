8a8aa89234adfb767cd7b6e1e4c837ef93efe9c5
Gabriel Kumlehn <gskumlehn@gmail.com>
2022-01-14 20:16:58 -0300

    adds mail functionality in contact.html in web-app blog

--- 7-web-app/blog/main.py
+++ 7-web-app/blog/main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
+EMAIL = 'YOUR_EMAIL_HERE'
+PASSWORD = 'YOUR_PASSWORD_HERE'
 post_data = requests.get("https://api.npoint.io/e826bb0d072e441eb66a").json()
 
 app = Flask(__name__)
@@ -13,9 +16,14 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
 
 @app.route('/post/<int:id>')
 def post(id):
@@ -23,8 +31,18 @@ def post(id):
     for post in post_data:
         if post["id"] == id:
             requested_post = post
-
     return render_template("post.html", id=id, post=requested_post)
+
+
+def send_email(name, email, phone, message):
+    msg = f"Subject:Blog message from {name} \n\n Your received a message in your blog" \
+          f"Message:{message}\n\nFrom: {name}\nEmail: {email}\nPhone: {phone}"
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=EMAIL,
+                            msg=msg)
 
 if __name__ == "__main__":
     app.run(debug=True)
